9a82d039a7b7de392481d43665a50c56cc1fa5b0
liparhams <linderrobin71@gmail.com>
2026-07-31 00:22:07 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,12 @@ import os
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-# Google Gemini
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# xAI Grok
+XAI_API_KEY = os.getenv("XAI_API_KEY")
 
 # Market
 SYMBOL = "XAUUSD"
+
 TIMEFRAMES = [
     "30m",
     "4h",
