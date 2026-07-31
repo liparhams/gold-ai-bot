a769feea77f7e3edf4e790331c3d69a7876f0b9d
liparhams <linderrobin71@gmail.com>
2026-07-31 19:22:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,46 @@
 import os
 
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 CHAT_ID = os.getenv("CHAT_ID")
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-MARKET_API_KEY = os.getenv("MARKET_API_KEY")
 
-SYMBOL = "XAU/USD"
+OPENROUTER_API_KEY = os.getenv(
+    "OPENROUTER_API_KEY"
+)
+
+
+MARKET_API_KEY = os.getenv(
+    "MARKET_API_KEY"
+)
+
+
+
+SYMBOL = "XAUUSD"
+
 
 
 TIMEFRAMES = {
+
     "30 دقیقه": "30min",
+
     "4 ساعته": "4h",
+
     "روزانه": "1day"
+
 }
 
 
+
+# اگر یکی قطع شد می‌رود بعدی
+
 AI_MODELS = [
+
     "meta-llama/llama-3.1-8b-instruct",
+
     "qwen/qwen-2.5-7b-instruct",
+
     "mistralai/mistral-7b-instruct"
+
 ]
