defac73865eb17e28631f1cd55c4a8d121a9cbdd
liparhams <linderrobin71@gmail.com>
2026-07-31 18:16:00 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,34 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID = os.environ.get("CHAT_ID")
 
-OPENROUTER_API_KEY = os.getenv(
+OPENROUTER_API_KEY = os.environ.get(
     "OPENROUTER_API_KEY"
+)
+
+MARKET_API_KEY = os.environ.get(
+    "MARKET_API_KEY"
 )
 
 
 MODEL = "google/gemini-2.0-flash-exp:free"
 
 
-MARKET_API_KEY = os.getenv(
-    "MARKET_API_KEY"
-)
+# چک امنیتی
+required = {
+    "BOT_TOKEN": BOT_TOKEN,
+    "CHAT_ID": CHAT_ID,
+    "OPENROUTER_API_KEY": OPENROUTER_API_KEY,
+    "MARKET_API_KEY": MARKET_API_KEY
+}
+
+
+for key,value in required.items():
+
+    if not value:
+        raise Exception(
+            f"Missing secret: {key}"
+        )
