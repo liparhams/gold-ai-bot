59d597422df2baec29d90a0220692e46597ecfb5
liparhams <linderrobin71@gmail.com>
2026-07-31 04:50:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
