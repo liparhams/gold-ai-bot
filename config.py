d13a07c704610e71c7d67b24a7fbfae7e3ebf20d
liparhams <linderrobin71@gmail.com>
2026-07-31 18:38:39 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,18 @@
 import os
 
 
-# Telegram
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
+OPENROUTER_API_KEY = os.environ.get(
+    "OPENROUTER_API_KEY"
+)
 
-# OpenRouter AI
-OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
+MARKET_API_KEY = os.environ.get(
+    "MARKET_API_KEY"
+)
 
 
-# Market Data
-MARKET_API_KEY = os.environ.get("MARKET_API_KEY")
-
-
-# AI fallback models
 MODELS = [
     "qwen/qwen-2.5-7b-instruct:free",
     "google/gemma-3-4b-it:free",
@@ -22,11 +20,9 @@ MODELS = [
 ]
 
 
-# Symbol
 SYMBOL = "XAU/USD"
 
 
-# Timeframes
 TIMEFRAMES = {
     "30 دقیقه": "30min",
     "4 ساعته": "4h",
@@ -34,9 +30,7 @@ TIMEFRAMES = {
 }
 
 
-# Chart settings
 CANDLE_COUNT = 150
 
 
-# Bot settings
 SIGNATURE = "\n\n@afinace - ai"
