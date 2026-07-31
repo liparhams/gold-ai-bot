388553f0b8121257c7775e629d5baac72309164b
liparhams <linderrobin71@gmail.com>
2026-07-31 17:35:59 +0330

    Update market.py

--- market.py
+++ market.py
@@ -2,79 +2,39 @@ import requests
 from config import MARKET_API_KEY
 
 
-def get_gold_data():
 
-    if not MARKET_API_KEY:
-        return "ERROR: MARKET_API_KEY is empty"
+def get_market(timeframe):
 
 
-    url = "https://api.twelvedata.com/time_series"
+    url="https://api.twelvedata.com/time_series"
 
 
-    params = {
+    params={
 
-        "symbol": "XAU/USD",
+        "symbol":"XAU/USD",
 
-        "interval": "4h",
+        "interval":timeframe,
 
-        "outputsize": 50,
+        "outputsize":100,
 
-        "apikey": MARKET_API_KEY
+        "apikey":MARKET_API_KEY
 
     }
 
 
-    try:
-
-        response = requests.get(
-            url,
-            params=params,
-            timeout=30
-        )
+    r=requests.get(
+        url,
+        params=params,
+        timeout=30
+    )
 
 
-        data = response.json()
+    data=r.json()
 
 
-        if "status" in data and data["status"] == "error":
+    if "values" not in data:
 
-            return (
-                "Twelve Data Error:\n"
-                + str(data)
-            )
+        raise Exception(data)
 
 
-        if "values" not in data:
-
-            return (
-                "No candle data:\n"
-                + str(data)
-            )
-
-
-        candles = data["values"]
-
-
-        result = """
-REAL XAUUSD 4H DATA
-
-"""
-
-
-        for candle in candles[:20]:
-
-            result += f"""
-Open: {candle['open']}
-High: {candle['high']}
-Low: {candle['low']}
-Close: {candle['close']}
-----------------
-"""
-
-
-        return result
-
-
-    except Exception as e:
-
-        return f"Market connection error: {e}"
+    return data["values"]
