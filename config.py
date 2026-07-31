e97b93e08dbe69a8f8f5b433c6db01ea152b2e5c
liparhams <linderrobin71@gmail.com>
2026-07-31 17:53:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,12 @@
 import os
 
 
-# Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 CHAT_ID = os.getenv("CHAT_ID")
 
 
-# OpenRouter
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
-MODEL = os.getenv(
-    "MODEL",
-    "google/gemini-2.5-flash"
-)
 
-
-# Market Data
-MARKET_API_KEY = os.getenv("MARKET_API_KEY")
+MODEL = "google/gemini-2.5-flash"
