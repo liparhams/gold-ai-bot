9a7bce66529e5883f72b669f3fd2b9d59025b712
liparhams <linderrobin71@gmail.com>
2026-07-31 18:31:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,30 +5,10 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 CHAT_ID = os.environ.get("CHAT_ID")
 
-OPENROUTER_API_KEY = os.environ.get(
-    "OPENROUTER_API_KEY"
-)
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
 
-MARKET_API_KEY = os.environ.get(
-    "MARKET_API_KEY"
-)
+MARKET_API_KEY = os.environ.get("MARKET_API_KEY")
 
 
-MODEL = "google/gemini-2.0-flash-exp:free"
-
-
-# چک امنیتی
-required = {
-    "BOT_TOKEN": BOT_TOKEN,
-    "CHAT_ID": CHAT_ID,
-    "OPENROUTER_API_KEY": OPENROUTER_API_KEY,
-    "MARKET_API_KEY": MARKET_API_KEY
-}
-
-
-for key,value in required.items():
-
-    if not value:
-        raise Exception(
-            f"Missing secret: {key}"
-        )
+# مدل AI
+MODEL = "meta-llama/llama-3.1-8b-instruct:free"
