44802a3d74ea3146593bc870f1f3a0f97f800c2e
liparhams <linderrobin71@gmail.com>
2026-07-31 19:20:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import asyncio
-import os
-from datetime import datetime
 
 from telegram import Bot
 
 from config import BOT_TOKEN, CHAT_ID, TIMEFRAMES
+
 from market import get_market
+
 from analysis import ai_analysis
+
 from chart import create_chart
 
+from news import get_today_news
 
 
-bot = Bot(
-    token=BOT_TOKEN
+
+bot=Bot(
+    BOT_TOKEN
 )
 
 
@@ -20,81 +23,47 @@ bot = Bot(
 async def send_analysis():
 
 
-    for name, tf in TIMEFRAMES.items():
+    for name,tf in TIMEFRAMES.items():
+
 
         try:
 
-            print(
-                "Getting data:",
-                name
-            )
+
+            df=get_market(tf)
 
 
-            df = get_market(tf)
-
-
-            image_path = create_chart(
+            image=create_chart(
                 df,
                 name
             )
 
 
-            analysis = ai_analysis(
+            text=ai_analysis(
                 df,
                 name
             )
 
 
-
-            # کپشن عکس کوتاه برای محدودیت تلگرام
-
-            caption_limit = 900
-
-
-            caption = f"""
+            caption=f"""
 📊 XAUUSD AI
 
-⏱ تایم فریم:
-{name}
+⏱ {name}
 
-{analysis[:caption_limit]}
+
+{text}
 """
 
 
+            if len(caption)>950:
+
+                caption=caption[:950]
+
+
             await bot.send_photo(
-
                 chat_id=CHAT_ID,
-
-                photo=open(
-                    image_path,
-                    "rb"
-                ),
-
+                photo=open(image,"rb"),
                 caption=caption
-
             )
-
-
-
-            # ادامه تحلیل اگر طولانی بود
-
-            if len(analysis) > caption_limit:
-
-
-                for i in range(
-                    caption_limit,
-                    len(analysis),
-                    3500
-                ):
-
-
-                    await bot.send_message(
-
-                        chat_id=CHAT_ID,
-
-                        text=analysis[i:i+3500]
-
-                    )
 
 
 
@@ -102,23 +71,26 @@ async def send_analysis():
 
 
             await bot.send_message(
-
                 chat_id=CHAT_ID,
-
-                text=f"""
-❌ خطا در {name}
-
-{str(e)}
-"""
-
+                text=f"❌ خطا در {name}\n{e}"
             )
 
 
 
+    # اخبار فقط یک بار
+
+    news=get_today_news()
 
 
-if __name__ == "__main__":
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=news
+    )
 
+
+
+
+if __name__=="__main__":
 
     asyncio.run(
         send_analysis()
