90eebffd8d73dbd2a7ad0282d04b83eeae4a15eb
liparhams <linderrobin71@gmail.com>
2026-07-31 18:10:40 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,21 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 CHAT_ID = os.getenv("CHAT_ID")
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-
-MARKET_API_KEY = os.getenv("MARKET_API_KEY")
-
-
-MODEL = "google/gemini-2.5-flash"
+OPENROUTER_API_KEY = os.getenv(
+    "OPENROUTER_API_KEY"
+)
 
 
-SYMBOL = "XAU/USD"
+MODEL = "google/gemini-2.0-flash-exp:free"
 
 
-TIMEFRAMES = [
-    ("30min", "30 دقیقه"),
-    ("4h", "4 ساعته"),
-    ("1day", "روزانه")
-]
+MARKET_API_KEY = os.getenv(
+    "MARKET_API_KEY"
+)
