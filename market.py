87ab61e2af7b15eb36ef9e447945c3158bc556a4
liparhams <linderrobin71@gmail.com>
2026-07-31 18:11:19 +0330

    Update market.py

--- market.py
+++ market.py
@@ -1,55 +1,59 @@
 import requests
-import pandas as pd
-
-from config import MARKET_API_KEY, SYMBOL
+from config import MARKET_API_KEY
 
 
 def get_market(interval):
+
 
     url = "https://api.twelvedata.com/time_series"
 
 
     params = {
-        "symbol": SYMBOL,
-        "interval": interval,
-        "outputsize": 120,
-        "apikey": MARKET_API_KEY
+
+        "symbol":"XAU/USD",
+
+        "interval":interval,
+
+        "outputsize":100,
+
+        "apikey":MARKET_API_KEY
+
     }
 
 
-    r = requests.get(
+    r=requests.get(
         url,
-        params=params,
-        timeout=30
+        params=params
     )
 
 
-    data = r.json()
+    data=r.json()
 
 
     if "values" not in data:
+
         raise Exception(data)
 
 
-    df = pd.DataFrame(data["values"])
+
+    candles=[]
 
 
-    df = df.rename(
-        columns={
-            "datetime":"time",
-            "open":"open",
-            "high":"high",
-            "low":"low",
-            "close":"close"
-        }
-    )
+    for x in data["values"]:
+
+        candles.append({
+
+            "time":x["datetime"],
+
+            "open":float(x["open"]),
+
+            "high":float(x["high"]),
+
+            "low":float(x["low"]),
+
+            "close":float(x["close"])
+
+        })
 
 
-    for c in ["open","high","low","close"]:
-        df[c] = df[c].astype(float)
-
-
-    df = df.iloc[::-1]
-
-
-    return df
+    return candles
