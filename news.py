8bd0dba8cd3b4cf7e0f21986cacc4020b25c3dbc
liparhams <linderrobin71@gmail.com>
2026-07-31 18:11:45 +0330

    Update news.py

--- news.py
+++ news.py
@@ -1,108 +1,89 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
-import pytz
 
 
 
 def get_today_news():
 
-    url = "https://www.forexfactory.com/calendar"
+
+    url="https://www.forexfactory.com/calendar"
 
 
-    headers = {
+
+    headers={
         "User-Agent":
         "Mozilla/5.0"
     }
 
 
+
     try:
 
-        r = requests.get(
+
+        r=requests.get(
             url,
             headers=headers,
             timeout=20
         )
 
 
-        soup = BeautifulSoup(
+        soup=BeautifulSoup(
             r.text,
             "lxml"
         )
 
 
-        news = []
+
+        result=[]
 
 
-        rows = soup.select(
+        for row in soup.select(
             "tr.calendar__row"
-        )
+        ):
 
 
-        for row in rows:
-
-            impact = row.select_one(
-                ".calendar__impact"
-            )
-
-
-            country = row.select_one(
+            country=row.select_one(
                 ".calendar__country"
             )
 
 
-            title = row.select_one(
+            impact=row.select_one(
+                ".calendar__impact"
+            )
+
+
+            event=row.select_one(
                 ".calendar__event"
             )
 
 
-            if not impact or not country or not title:
+
+            if not country or not impact or not event:
                 continue
 
 
 
-            # فقط آمریکا
+            if "USD" in country.text:
 
-            if "USD" not in country.text:
-                continue
-
+                if "high" in str(impact).lower():
 
 
-            # فقط قرمز
-
-            if "high" not in str(impact).lower():
-                continue
+                    result.append(
+                        "🔴 "+event.text.strip()
+                    )
 
 
 
-            news.append(
-                {
-                "country":"USD",
-                "event":title.text.strip()
-                }
-            )
+        if not result:
+
+            return "امروز خبر قرمز مهم آمریکا وجود ندارد."
 
 
-        if not news:
 
-            return "امروز خبر قرمز مهم آمریکا پیدا نشد."
-
-
-        text = "📰 اخبار مهم امروز آمریکا:\n\n"
-
-
-        for n in news:
-
-            text += (
-                f"🔴 {n['event']}\n"
-                f"اثر احتمالی: بررسی روی دلار و XAUUSD\n\n"
-            )
-
-
-        return text
+        return "\n".join(result)
 
 
 
     except Exception as e:
 
-        return f"خطا در اخبار: {e}"
+        return "خطای خبر: "+str(e)
