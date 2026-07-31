7891061dc2b1224eff4b65159242277712780c1b
liparhams <linderrobin71@gmail.com>
2026-07-31 18:37:26 +0330

    Update market.py

--- market.py
+++ market.py
@@ -1,59 +1,70 @@
 import requests
-from config import MARKET_API_KEY
+import pandas as pd
+
+from config import MARKET_API_KEY, SYMBOL
 
 
-def get_market(interval):
-
+def get_market(timeframe):
 
     url = "https://api.twelvedata.com/time_series"
 
 
     params = {
-
-        "symbol":"XAU/USD",
-
-        "interval":interval,
-
-        "outputsize":100,
-
-        "apikey":MARKET_API_KEY
-
+        "symbol": SYMBOL,
+        "interval": timeframe,
+        "outputsize": 200,
+        "apikey": MARKET_API_KEY
     }
 
 
-    r=requests.get(
+    response = requests.get(
         url,
-        params=params
+        params=params,
+        timeout=30
     )
 
 
-    data=r.json()
+    data = response.json()
 
 
     if "values" not in data:
-
-        raise Exception(data)
-
-
-
-    candles=[]
-
-
-    for x in data["values"]:
-
-        candles.append({
-
-            "time":x["datetime"],
-
-            "open":float(x["open"]),
-
-            "high":float(x["high"]),
-
-            "low":float(x["low"]),
-
-            "close":float(x["close"])
-
-        })
+        raise Exception(
+            data.get(
+                "message",
+                str(data)
+            )
+        )
 
 
-    return candles
+    df = pd.DataFrame(
+        data["values"]
+    )
+
+
+    df["datetime"] = pd.to_datetime(
+        df["datetime"]
+    )
+
+
+    df = df.sort_values(
+        "datetime"
+    )
+
+
+    for col in [
+        "open",
+        "high",
+        "low",
+        "close"
+    ]:
+        df[col] = pd.to_numeric(
+            df[col]
+        )
+
+
+    df = df.set_index(
+        "datetime"
+    )
+
+
+    return df
