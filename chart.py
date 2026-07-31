f6947576c35f88d20d53c1fa57f1f2780fc10e76
liparhams <linderrobin71@gmail.com>
2026-07-31 18:03:20 +0330

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,4 +1,5 @@
 import mplfinance as mpf
+import pandas as pd
 
 
 def create_chart(df, name):
@@ -6,18 +7,48 @@ def create_chart(df, name):
     file = f"{name}.png"
 
 
-    df2 = df.copy()
+    chart = df.copy()
 
-    df2.index = df2["time"]
+
+    # تبدیل زمان به تاریخ
+    chart["time"] = pd.to_datetime(
+        chart["time"]
+    )
+
+
+    # قرار دادن زمان به عنوان index
+    chart = chart.set_index(
+        "time"
+    )
+
+
+    # مرتب سازی
+    chart = chart.sort_index()
+
+
+    # فقط ستون های لازم
+    chart = chart[
+        [
+            "open",
+            "high",
+            "low",
+            "close"
+        ]
+    ]
 
 
     mpf.plot(
-        df2,
+        chart,
         type="candle",
         style="charles",
         title=f"XAUUSD {name}",
+        figsize=(12,6),
         volume=False,
-        savefig=file
+        savefig=dict(
+            fname=file,
+            dpi=150,
+            bbox_inches="tight"
+        )
     )
 
 
