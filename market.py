7d0741fe8f158e258c4e4b6b45dfd77879ac296e
liparhams <linderrobin71@gmail.com>
2026-07-31 17:54:29 +0330

    Update market.py

--- market.py
+++ market.py
@@ -1,14 +1,9 @@
 import requests
+
 from config import MARKET_API_KEY
 
 
-def get_market(timeframe):
-
-    if not MARKET_API_KEY:
-        raise Exception(
-            "❌ MARKET_API_KEY در Secret یا config وجود ندارد"
-        )
-
+def get_market(interval):
 
     url = "https://api.twelvedata.com/time_series"
 
@@ -17,63 +12,28 @@ def get_market(timeframe):
 
         "symbol": "XAU/USD",
 
-        "interval": timeframe,
+        "interval": interval,
 
-        "outputsize": 200,
+        "outputsize": 100,
 
         "apikey": MARKET_API_KEY
 
     }
 
 
-    try:
-
-        response = requests.get(
-            url,
-            params=params,
-            timeout=30
-        )
+    r = requests.get(
+        url,
+        params=params,
+        timeout=30
+    )
 
 
-        data = response.json()
+    data = r.json()
 
 
+    if "values" not in data:
 
-        if "values" not in data:
-
-            raise Exception(
-                f"Twelve Data Error: {data}"
-            )
+        raise Exception(data)
 
 
-
-        candles = []
-
-
-        for item in reversed(data["values"]):
-
-            candles.append({
-
-                "datetime": item["datetime"],
-
-                "open": float(item["open"]),
-
-                "high": float(item["high"]),
-
-                "low": float(item["low"]),
-
-                "close": float(item["close"])
-
-            })
-
-
-
-        return candles
-
-
-
-    except requests.exceptions.RequestException as e:
-
-        raise Exception(
-            f"خطای اتصال به Market API: {e}"
-        )
+    return data["values"]
