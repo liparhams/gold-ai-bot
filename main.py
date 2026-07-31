965e3cfa93c0626f1ac033f75d62811bbc6349ec
liparhams <linderrobin71@gmail.com>
2026-07-31 19:24:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,68 +2,88 @@ import asyncio
 
 from telegram import Bot
 
-from config import BOT_TOKEN, CHAT_ID, TIMEFRAMES
+from config import (
+    BOT_TOKEN,
+    CHAT_ID,
+    TIMEFRAMES
+)
 
 from market import get_market
 
-from analysis import ai_analysis
-
 from chart import create_chart
+
+from analysis import ai_analysis
 
 from news import get_today_news
 
 
 
-bot=Bot(
+bot = Bot(
     BOT_TOKEN
 )
 
 
 
-async def send_analysis():
+async def send_bot():
+
 
 
     for name,tf in TIMEFRAMES.items():
 
-
         try:
 
 
-            df=get_market(tf)
+            df = get_market(tf)
 
 
-            image=create_chart(
+
+            image = create_chart(
                 df,
                 name
             )
 
 
-            text=ai_analysis(
+
+            analysis = ai_analysis(
                 df,
                 name
             )
 
 
-            caption=f"""
+
+            caption = f"""
 📊 XAUUSD AI
 
-⏱ {name}
+⏱ تایم فریم: {name}
 
 
-{text}
+{analysis}
 """
 
 
-            if len(caption)>950:
 
-                caption=caption[:950]
+            # محدودیت تلگرام
+            if len(caption) > 900:
+
+                caption = caption[:900]
 
 
-            await bot.send_photo(
-                chat_id=CHAT_ID,
-                photo=open(image,"rb"),
-                caption=caption
-            )
+
+            with open(
+                image,
+                "rb"
+            ) as photo:
+
+
+                await bot.send_photo(
+
+                    chat_id=CHAT_ID,
+
+                    photo=photo,
+
+                    caption=caption
+
+                )
 
 
 
@@ -71,27 +91,39 @@ async def send_analysis():
 
 
             await bot.send_message(
+
                 chat_id=CHAT_ID,
-                text=f"❌ خطا در {name}\n{e}"
+
+                text=f"""
+❌ خطا در {name}
+
+{e}
+"""
+
             )
 
 
 
-    # اخبار فقط یک بار
 
-    news=get_today_news()
+    # فقط یک پیام اخبار
+
+    news = get_today_news()
 
 
     await bot.send_message(
+
         chat_id=CHAT_ID,
+
         text=news
+
     )
 
 
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
+
 
     asyncio.run(
-        send_analysis()
+        send_bot()
     )
