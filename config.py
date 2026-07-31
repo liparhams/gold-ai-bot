f675a72539b1dfa481d0253dcf4e5f5f4bc56d69
liparhams <linderrobin71@gmail.com>
2026-07-31 17:08:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
 import os
 
+# Telegram Bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Telegram Channel / Group ID
 CHAT_ID = os.getenv("CHAT_ID")
+
+
+# OpenRouter API
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
+
+# AI Model
 MODEL = os.getenv(
     "MODEL",
-    "google/gemini-2.0-flash-001"
+    "google/gemini-2.5-flash"
 )
+
+
+# OpenRouter URL
+OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
