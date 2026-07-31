86d0595b258c1c88da0bb7a0bbeb9c7e8e8e3773
liparhams <linderrobin71@gmail.com>
2026-07-31 05:17:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,7 @@
-import os
-from dotenv import load_dotenv
+BOT_TOKEN = "اینجا توکن ربات تلگرام"
 
-# خواندن متغیرهای محیطی
-load_dotenv()
+CHAT_ID = "اینجا آیدی مقصد"
+# مثال کانال:
+# CHAT_ID = "-1001234567890"
 
-
-# =========================
-# Telegram Settings
-# =========================
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-
-# =========================
-# OpenRouter Settings
-# =========================
-
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-
-
-# مدل هوش مصنوعی
-MODEL = "meta-llama/llama-3.1-8b-instruct"
-
-
-# =========================
-# Check Settings
-# =========================
-
-if not BOT_TOKEN:
-    raise Exception("BOT_TOKEN خالی است")
-
-if not CHAT_ID:
-    raise Exception("CHAT_ID خالی است")
-
-if not OPENROUTER_API_KEY:
-    raise Exception("OPENROUTER_API_KEY خالی است")
+OPENROUTER_API_KEY = "اینجا api اوپن روتر"
